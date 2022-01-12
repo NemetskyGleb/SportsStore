0f96ab51b652326cb40099e781c8a15395e4c6e6
Gleb <glebdvp@gmail.com>
2022-01-12 08:07:09 +0700

    Создан контекст базы данных. Его настройка была выполнена в Startup.cs

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -7,16 +7,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
 
 namespace ProgamFeatures
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        private IConfiguration Configuration { get; set; }
+        // The constructor receives an IConfiguration object through its
+        // constructor and assigns it to the Configuration property, which is used to access the connection string.
+        // Entity Framework Core is configured with the AddDbContext method, which registers the database context class and configures
+        // the relationship with the database. The UseSQLServer method declares that SQL Server is being used and the connection string is
+        // read via the IConfiguration object.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(opts => {
+                opts.UseSqlServer(
+                    Configuration["ConnectionString:SportsStoreConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
