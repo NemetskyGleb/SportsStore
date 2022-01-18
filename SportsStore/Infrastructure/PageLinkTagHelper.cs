3de5d4afa23f8bf01855be98f5c8c715bb1ffa10
Gleb <glebdvp@gmail.com>
2022-01-18 08:17:08 +0700

    Добавлен стилизация с помощью Bootstrap

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -20,6 +20,10 @@ public class PageLinkTagHelper : TagHelper
     public ViewContext ViewContext { get; set; }
     public PagingInfo PageModel { get; set; }
     public string PageAction { get; set; }
+    public bool PageClassesEnabled { get; set; } = false;
+    public string PageClass { get; set; }
+    public string PageClassNormal { get; set; }
+    public string PageClassSelected { get; set; }
     public override void Process(TagHelperContext context,
             TagHelperOutput output)
     {
@@ -30,8 +34,13 @@ public class PageLinkTagHelper : TagHelper
             TagBuilder tag = new TagBuilder("a");
             tag.Attributes["href"] = urlHelper.Action(PageAction,
                 new { productPage = i });
-                tag.InnerHtml.Append(i.ToString());
-                result.InnerHtml.AppendHtml(tag);
+            if (PageClassesEnabled) {
+                tag.AddCssClass(PageClass);
+                tag.AddCssClass(i == PageModel.CurrentPage
+                ? PageClassSelected : PageClassNormal);
+            }
+            tag.InnerHtml.Append(i.ToString());
+            result.InnerHtml.AppendHtml(tag);
         }
         output.Content.AppendHtml(result.InnerHtml);
     }
